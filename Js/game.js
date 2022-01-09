61316252a7b96277a49bf6314c7756befd392795
iamdev-james <jamesoluwabukola87@gmail.com>
2022-01-09 00:09:47 -0100

    Initial commit

---
+++ Js/game.js
@@ -0,0 +1,28 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// const light = new THREE.AmbientLight( 0xffffff );
+// scene.add( light );
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 5;
+
+const loader = new THREE.GLTFLoader();
+loader.load("../models/scene.gltf", function(gltf) {
+    scene.add( gltf.scene );
+    gltf.scene.scale.set( .4, .4, .4 )
+})
+
+function animate() {
+	renderer.render( scene, camera ); 
+	requestAnimationFrame( animate );
+}
+animate();
